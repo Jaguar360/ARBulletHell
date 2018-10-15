be3da68beaef5f1086151060c45d2836e283049a
Jelani <jhp@js-macbook-pro.resource.campus.njit.edu>
2018-10-15 12:10:53 -0400

    Created PlaceObjects scene
    Created PlaceObjects scene for testing PointCloud object spawning. Following AR Portal Tutorial by Guidev on YT.

--- AR Bullet Hell/Assets/Scripts/ARController.cs
+++ AR Bullet Hell/Assets/Scripts/ARController.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 using GoogleARCore;
 
+#if UNITY_EDITOR
+using input = GoogleARCore.InstantPreviewInput;
+#endif
+
 public class ARController : MonoBehaviour 
 {
 	private List<DetectedPlane> m_newTrackedPlanes = new List<DetectedPlane>();
 
 	public GameObject GridPrefab;
+	public GameObject Portal;
+	public GameObject ARCamera;
+
 	// Use this for initialization
 	void Start () 
 	{
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () 
 	{
@@ -29,6 +36,32 @@ public class ARController : MonoBehaviour
 			GameObject grid = Instantiate(GridPrefab, Vector3.zero, Quaternion.identity, transform);
 
 			grid.GetComponent<GridVisualizer>().Initialize(m_newTrackedPlanes[i]);
+		}
+
+		// Check if user touches the screen
+		Touch touch;
+		if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began) 
+		{
+			return;
+		}
+
+		// Check if user touched a detected plane
+		TrackableHit hit;
+		if (Frame.Raycast(touch.position.x, touch.position.y, TrackableHitFlags.PlaneWithinPolygon, out hit)) 
+		{
+			// place object on top of detected plane
+			Portal.SetActive(true);
+
+			Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+			Portal.transform.position = hit.Pose.position;
+			Portal.transform.rotation = hit.Pose.rotation;
+
+            // have placed objected face camera
+			Vector3 cameraPosition = ARCamera.transform.position;
+			cameraPosition.y = hit.Pose.position.y;
+			Portal.transform.LookAt(cameraPosition, Portal.transform.up);
+			Portal.transform.parent = anchor.transform;
 		}
 	}
 }
