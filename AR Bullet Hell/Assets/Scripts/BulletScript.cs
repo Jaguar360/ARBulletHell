a1a4a83dd62760c2eb617c1ddaa02f1e82df5116
Timothy Nieto <tjn9@njit.edu>
2018-10-24 17:36:38 -0400

    Bullet and Gun working, attached to Fire Button works!

---
+++ AR Bullet Hell/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float speed = 0f;
+    public float lifeDuration = 2f;
+    public float lifeTimer;
+
+    // Use this for initialization
+    void Start()
+    {
+        lifeTimer = lifeDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        lifeTimer -= Time.deltaTime;
+
+        if (lifeTimer <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ AR Bullet Hell/Assets/Scripts/GunScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public GameObject projectile;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  if (Input.GetMouseButtonDown(0))
+       // {
+       //     Shoot();
+       // }
+    }
+
+    public void Shoot()
+    {
+        GameObject bullet = Instantiate(projectile);
+        bullet.transform.position = player.transform.position + player.transform.forward;
+        bullet.transform.forward = player.transform.forward;
+    }
+}
