50e806f3cd5fd9127a7f42c59bd458d6dd2d5e92
Jaguar360 <jaguars310@msn.com>
2018-10-27 12:16:12 -0400

    Made improvements to AR scene

--- AR Bullet Hell/Assets/Scripts/Enemy.cs
+++ AR Bullet Hell/Assets/Scripts/Enemy.cs
@@ -38,14 +38,12 @@ public class Enemy : MonoBehaviour {
     
 	public IEnumerator Shoot()
     {
-		Debug.Log("shoot begin");
 		while (gameManager.IsGameOver() == false) 
 		{
             yield return new WaitForSeconds(3);
             GameObject bullet = (GameObject)Instantiate(projectile);
             bullet.transform.position = transform.position + transform.forward;
             bullet.transform.forward = transform.forward;
-			Debug.Log("shooteth");
 		}
     }
 }
